bfe3db3097d1dd16d88c66e5c289ff334ac1c9a6
javvidar004 <157633880+javvidar004@users.noreply.github.com>
2025-03-24 17:57:17 -0600

    Update urls

--- sm_ads/urls.py
+++ sm_ads/urls.py
@@ -4,4 +4,15 @@ from . import views
 urlpatterns = [
     path('', views.main, name='main'),
     path('index.html', views.main, name='main'),
+    
+    path('smp/<int:id>', views.socialMediaPlatform, name='detailPlatform'),
+    path('cntr/<int:id>', views.countriesDetail, name='detailContry'),
+    path('gnr/<int:id>', views.genderDetail, name='detailGender'),
+    path('entr/<int:id>', views.entertainmentDetail, name='detailEntertainment'),
+    
+    path('general.html', views.general, name='general'),
+
+
+
+    #path('get_chart1/', views.get_chart1, name='get_chart1')
 ]
