3884f3481b06e75e938e4a05858050a554cea4c0
javvidar004 <157633880+javvidar004@users.noreply.github.com>
2025-03-25 16:04:24 -0600

    Update views.py

--- sm_ads/views.py
+++ sm_ads/views.py
@@ -1,16 +1,24 @@
 from django.http import HttpResponse
+from django.template import loader
+from .models import SocialMedia
+
+from django.http import HttpResponse
 from django.http.response import JsonResponse
 from django.template import loader
 from random import randrange
 from .models import *
 
-
-def main(request):
-  prefEntre = Entretaiment.objects.raw('SELECT entretaiment.entertainment_id AS entertainment_id, entretaiment.entertainment_name AS name, COUNT(preferred_content_id) AS people FROM entretaiment, users WHERE preferred_content_id = entretaiment.entertainment_id GROUP BY entretaiment.entertainment_id;')
+def auxMenu():
   redesSociales = SocialMedia.objects.all().values()
   paises = Countries.objects.all().values()
   generos = Gender.objects.all().values()
   tipoEntretenimiento = Entretaiment.objects.all().values()
+  return redesSociales, paises, generos, tipoEntretenimiento
+
+
+def main(request):
+  prefEntre = Entretaiment.objects.raw('SELECT entretaiment.entertainment_id AS entertainment_id, entretaiment.entertainment_name AS name, COUNT(preferred_content_id) AS people FROM entretaiment, users WHERE preferred_content_id = entretaiment.entertainment_id GROUP BY entretaiment.entertainment_id;')
+  redesSociales, paises, generos, tipoEntretenimiento = auxMenu()
   template = loader.get_template('index.html') 
   context = {
      'prefEntre' : prefEntre,
@@ -24,33 +32,69 @@ def main(request):
 
   #path('smp/<int:id>', views.socialMediaPlatform, name=detailPlatform)
 def socialMediaPlatform(request, id):
-    template = loader.get_template()
+    redesSociales, paises, generos, tipoEntretenimiento = auxMenu()
+    template = loader.get_template('smp.html')
+    titulo = ''
+    for i in redesSociales:
+       if id == i['socialm_id']:
+          titulo = i['socialm_name']
     context = {
-
+     'redesSociales': redesSociales,
+     'paises': paises,
+     'generos': generos,
+     'tipoEntretenimiento': tipoEntretenimiento,
+     'titulo': titulo.upper(),
     }
     return HttpResponse(template.render(context, request))
   
   #path('cntr/<int:id>', views.countriesDetail, name=detailContry)
 def countriesDetail(request, id):
-    template = loader.get_template()
+    redesSociales, paises, generos, tipoEntretenimiento = auxMenu()
+    template = loader.get_template('paises.html')
+    titulo = ''
+    for i in paises:
+       if id == i['country_id']:
+          titulo = i['country_name']
     context = {
-
+      'redesSociales': redesSociales,
+      'paises': paises,
+      'generos': generos,
+      'tipoEntretenimiento': tipoEntretenimiento,
+      'titulo': titulo.upper(),
     }
     return HttpResponse(template.render(context, request))
 
   #path('gnr/<int:id>', views.genderDetail, name=detailGender)
 def genderDetail(request, id):
-    template = loader.get_template()
+    redesSociales, paises, generos, tipoEntretenimiento = auxMenu()
+    template = loader.get_template('generos.html')
+    titulo = ''
+    for i in generos:
+       if id == i['gender_id']:
+          titulo = i['gender']
     context = {
-
+       'redesSociales': redesSociales,
+       'paises': paises,
+       'generos': generos,
+       'tipoEntretenimiento': tipoEntretenimiento,
+       'titulo': titulo.upper(),
     }
     return HttpResponse(template.render(context, request))
 
   #path('entr/<int:id>', views.entertainmentDetail, name=detailEntertainment)
 def entertainmentDetail(request, id):
-    template = loader.get_template()
+    redesSociales, paises, generos, tipoEntretenimiento = auxMenu()
+    template = loader.get_template('entretenimiento.html')
+    titulo = ''
+    for i in tipoEntretenimiento:
+       if id == i['entertainment_id']:
+          titulo = i['entertainment_name']
     context = {
-
+        'redesSociales': redesSociales,
+        'paises': paises,
+        'generos': generos,
+        'tipoEntretenimiento': tipoEntretenimiento,
+        'titulo': titulo.upper(),
     }
     return HttpResponse(template.render(context, request))
 
@@ -101,5 +145,6 @@ def get_chart1(_request):
 
     return JsonResponse(chart)
 '''
+
 
 # Create your views here.
